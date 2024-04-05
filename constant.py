a06ae16f5088004fee6b773eaa64452383ce3b70
Laura Burn <lauraburn@talktalk.net>
2024-04-05 17:13:33 +1300

    Parameter space class made

--- constant.py
+++ constant.py
@@ -3,11 +3,15 @@
 import numpy as np
 import scipy as sp
 import astropy as ap
+import astropy.constants
+import astropy.units
 
 # all SI units
-#YR = ap.units.year.to(ap.units.s)   # convert year to seconds
+PI = np.pi
+YR = ap.units.year.to(ap.units.s)   # convert year to seconds
 MSOL = ap.constants.M_sun.value     # mass of sun
 PC = ap.constants.pc.value          # parsec
-MPC = 1.0e6*PC                      # mega-parsec
+MPC = 1.0e6*PC               # mega-parsec
 NG = ap.constants.G.value           # Newton's G
 LC = ap.constants.c.value           # speed of light
+
--- framework.py
+++ framework.py
@@ -1,4 +1,6 @@
-import constants
+import numpy as np
+
+import constant as const
 
 # create a class which defines the parameter space
 # will have 3/4 arguments - z, m, q, f
@@ -34,16 +36,16 @@ import constants
         self.redz = redz
 """
 # and they then specify the shape sam = sam.Semi_Analytic_Model(shape=30)
-
+'''
 class ParameterGrid:
     def __init__(self, mtot, mrat, redz, shape=None):
-        self.mtot = mtot
+        self.masstot = mtot
         self.mrat = mrat
         self.redz = redz
         self.shape = shape
 
     def create_grid(self):
-        params = [self.mtot, self.mrat, self.redz]
+        params = [mtot, mrat, redz]
         param_names = ['mtot', 'mrat', 'redz']
         grids = []
 
@@ -58,7 +60,7 @@ class ParameterGrid:
 
         return grids
 
-# Example usage:
+
 mtot = (1, 100, 10)  # Log-spaced between 1 and 100, with 10 points
 mrat = (0.1, 0.9, 5)  # Log-spaced between 0.1 and 0.9, with 5 points
 redz = (0, 2, 8)  # Log-spaced between 0 and 2, with 8 points
@@ -66,3 +68,54 @@ shape = [None, None, None]  # Shape of the parameter space, None for automatic c
 
 param_grid = ParameterGrid(mtot, mrat, redz, shape)
 grids = param_grid.create_grid()
+print(grids)
+'''
+
+## Might be better to not define a rigid grid and instead handle parameters dynamically
+## so we can change the resolution in different parts more easily
+## holodeck produces a fixed grid like this 
+
+class ParameterSpace:
+    # the __init__ function is always executed when the class is initiated
+    # self individualises an object, so each name generated is it's own
+    def __init__(self, mass, mass_ratio, redshift, size):
+        self.mass = mass
+        self.mass_ratio = mass_ratio
+        self.redshift = redshift
+        self.size = size
+
+    # * unpacks the elements of the iterables as separate arguments
+    # so if mass = (1, 10), it unpacks them into 1 and 10 and gives them to np.logspace as arguments
+    # num = number of samples to generate
+    def generate_grid(self):
+        mass_params = np.logspace(*self.mass, num = self.size[0])
+        mass_ratio_params = np.logspace(*self.mass_ratio, num = self.size[1])
+        redshift_params = np.logspace(*self.redshift, num = self.size[2])
+
+        print("mass: ", mass_params)
+        print('mass ratio: ', mass_ratio_params)
+        print('redshift: ', redshift_params)
+
+        self.grid = np.meshgrid(mass_params, mass_ratio_params, redshift_params, indexing='ij')
+
+    def get_grid(self):
+        # checks if grid has been generated (hasattr) and generates it if it hasn't (self.generate_grid())
+        if not hasattr(self, 'grid'):
+            self.generate_grid()
+        # returns the grid stored in the grid attribute of the instance
+        return self.grid
+    
+# Example usage:
+mass_range = np.log10((1e6, 1e9))  # Define the range for mass 
+mass_ratio_range = np.log10((0.1, 1))  # Define the range for mass ratio 
+redshift_range = np.log10((1, 5))  # Define the range for redshift 
+grid_shape = (5, 5, 5)  # Define the shape of the grid
+
+# Instantiate the ParameterGrid class
+param_grid = ParameterSpace(mass_range, mass_ratio_range, redshift_range, grid_shape)
+
+# Get the grid
+grid = param_grid.get_grid()
+
+#print(grid)
+
